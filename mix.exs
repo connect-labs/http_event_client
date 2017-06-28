2b9a661b74d100449dc72f0a18acb5f3c6f3cc9b
Jake Humphrey <jake@codelation.com>
2017-06-27 21:37:07 -0500

    changing default message to nil

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule HTTPEventClient.Mixfile do
      description: description(),
      package: package(),
      source_url: "https://github.com/humphreyja/http_event_client",
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
